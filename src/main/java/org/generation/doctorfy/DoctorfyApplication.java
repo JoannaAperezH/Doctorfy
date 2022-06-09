b7e5b5e854034b3ad699f340b02c9c3a83697077
joannaPerez <joannaa.perezh@gmail.com>
2022-06-09 15:07:02 -0500

    Estructura inicial backend doctorfy

---
+++ src/main/java/org/generation/doctorfy/DoctorfyApplication.java
@@ -0,0 +1,13 @@
+package org.generation.doctorfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorfyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/doctorfy/DoctorfyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.doctorfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorfyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
