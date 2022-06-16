50e9091eb428c5046f6ce079a199715b27652eca
Diego Briones <dbrionesmtz@yahoo.com>
2022-06-15 22:57:56 -0500

    Creando la funcion para registrar doctores

--- src/main/java/org/generation/doctorfy/controller/UsuarioController.java
+++ src/main/java/org/generation/doctorfy/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import org.generation.doctorfy.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,10 @@ public final UsuarioService usuarioService;
 	@GetMapping
 	public List<Usuario> getUsuarios() {
 		return this.usuarioService.getUsuarios();
+	}
+	
+	@PostMapping(path = "signIn")
+	public void signIn(@RequestBody Usuario usuario) {
+		this.usuarioService.signIn(usuario);
 	}
 }
