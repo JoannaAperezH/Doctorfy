3b4d57b8589d5a8e1627b0e4b267ef12f888c673
Diego Briones <dbrionesmtz@yahoo.com>
2022-06-16 22:02:32 -0500

    terminando metodo de completarPerfil

---
+++ src/main/java/org/generation/doctorfy/service/EnfermedadEspecialidadRepository.java
@@ -0,0 +1,8 @@
+package org.generation.doctorfy.service;
+
+import org.generation.doctorfy.model.EnfermedadEspecialidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnfermedadEspecialidadRepository extends JpaRepository<EnfermedadEspecialidad, Long>{
+
+}
